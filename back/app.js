adbc15c9ecc07d458bf5e5e818f0cac8d28de80c
Mathieu Franchi <89087885+Mathieu-Franchi@users.noreply.github.com>
2022-05-15 16:29:48 +0200

    REMOVED MONGODB CONNEXION

--- back/app.js
+++ back/app.js
@@ -9,7 +9,7 @@ app.use(express.json());
 
 /* MONGODB */
 const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://Mathieu:M4815162342@cluster0.delpt.mongodb.net/Cluster0?retryWrites=true&w=majority";
+
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 client.connect(err => {
   const collection = client.db("Cluster0").collection("piquante");
